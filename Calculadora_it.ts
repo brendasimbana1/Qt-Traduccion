818a3ec6541ac97c3d3164a952dd72c8a9fe6584
Brenda Simbaña <bsimbanag1@est.ups.edu.ec>
2023-01-25 12:33:49 -0500

    Se agrego italiano

---
+++ Calculadora_it.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Principal</name>
+    <message>
+        <location filename="principal.ui" line="14"/>
+        <source>Calculadora</source>
+        <translation>Calcolatrice</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="23"/>
+        <source>Número 1:</source>
+        <translation>Numero 1:</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="41"/>
+        <source>Número 2:</source>
+        <translation>Numero 2:</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="59"/>
+        <source>Operación:</source>
+        <translation>Operazione:</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="67"/>
+        <source>Sumar</source>
+        <translation>Aggiungere</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="72"/>
+        <source>Restar</source>
+        <translation>Sottrazione</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="77"/>
+        <source>Multiplicar</source>
+        <translation>Moltiplicare</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="82"/>
+        <source>Dividir</source>
+        <translation>Diviso</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="107"/>
+        <source>Calcular</source>
+        <translation>Calcolare</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="126"/>
+        <source>Archivo</source>
+        <translation>Archivio</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="134"/>
+        <source>Salir</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="40"/>
+        <source>Resultado</source>
+        <translation>Risultato</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="41"/>
+        <source>El resultado es: %1</source>
+        <translation>Il risultato è: %1</translation>
+    </message>
+</context>
+</TS>
